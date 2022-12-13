eb3ce86fc09468c7ee1d26097add3d6d01d74b4e
Sunday <sundayoruwhone@gmail.com>
2022-12-13 22:40:20 +0100

    Done

---
+++ client/src/config.ts
@@ -0,0 +1,19 @@
+// const apiId = 'vbpb0zm6s6'
+// export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+// export const authConfig = {
+//   domain: 'dev-m4nwauddejm0iliz.us.auth0.com',
+//   clientId: '7T5ULeFp7Ts7qGT5Gs4FLvun24i2vPHX',
+//   callbackUrl: 'http://localhost:3000/callback'
+// }
+
+const apiId = '638ec0cabf8e07fd9e023ca7'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  // TODO: Create an Auth0 application and copy values from it into this map. For example:
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-csd2kjw0eenlxvdn.us.auth0.com', // Auth0 domain
+  clientId: 'aWgcNP2LtAPI54tSXgo2vl5JdeZAMRV6', // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
